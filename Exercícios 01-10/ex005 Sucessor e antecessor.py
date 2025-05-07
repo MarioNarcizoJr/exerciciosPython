1839669feafd2db991d1bb7b3397edb0f2fd6d36
Mário Narcizo Júnior <129921507+MarioNarcizoJr@users.noreply.github.com>
2025-05-06 23:41:55 -0300

    Add files via upload

---
+++ Exercícios 01-10/ex005 Sucessor e antecessor.py
@@ -0,0 +1,2 @@
+numero = int(input('Digite um número: '))
+print(f'Para o número {numero} temos que, seu sucessor é {numero + 1} e seu antecessor é {numero - 1}.')
---
+++ Exercícios 01-10/ex006 Dobro, Triplo e raiz quadrada.py
@@ -0,0 +1,3 @@
+numero = int(input('Digite um número: '))
+print(f'O dobro do número {numero} é {numero*2}, seu triplo é {numero*3} e a raiz quadrada vale {numero**0.5:.2f}.')
+
